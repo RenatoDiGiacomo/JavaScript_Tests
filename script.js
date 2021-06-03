d7c070cce86de6824ec5ad3ae58cd99992e0c319
RenatoDiGiacomo <renato_di_giacomo@hotmail.com>
2021-06-03 19:28:32 -0300

    Menu Update

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
+function initList() {
+    const menueach = function() {
+        const menuItems = [
+            "Counter",
+            "Hex",
+            "NavBar",
+            "Progress-Bar",
+            "Scrolling Bar Top",
+            "Typing Writer",
+        ];
+        let print = "";
 
-const menueach = function () {
-  const menuItems = [
-    "Counter",
-    "Hex",
-    "NavBar",
-    "Progress-Bar",
-    "Scrolling Bar Top",
-    "Typing Writer",
-  ];
-  let print = "";
+        menuItems.forEach(item => {
+            const text = item.replace(/\s/g, '-').toLowerCase()
+            print += `<li><a href="${text}/index.html">${item}</a></li>`;
+        })
+        return print;
+    };
 
-  menuItems.forEach(item =>{
-      const text = item.replace(/\s/g, '-').toLowerCase()
-    print += `<li><a href="${text}/index.html">${item}</a></li>`;
-  })
-  return print;
-};
+    const menu = document.getElementById("menu");
+    menu.innerHTML = menueach()
 
-const menu = document.getElementById("menu");
-menu.innerHTML = menueach()
+}
+initList()
+const
