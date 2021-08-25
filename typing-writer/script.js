3edd3a05a8166265d45d8efc23ff0e5ce90c0dec
RenatoDiGiacomo <renato_di_giacomo@hotmail.com>
2021-08-24 22:01:22 -0300

    Typewriter update

--- typing-writer/script.js
+++ typing-writer/script.js
@@ -1,13 +1,12 @@
-let text1 = 'Renato'
 const p = document.querySelector('p')
-const text = `Lorem ipsum ${text1}, dolor sit amet consectetur adipisicing elit. Quod illo doloremque nesciunt doloribus molestiae dolore aliquam voluptatum fuga earum, assumenda impedit quaerat, voluptates dolorem! Magni libero autem porro commodi fugit?`
+const text = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod illo doloremque nesciunt doloribus molestiae dolore aliquam voluptatum fuga earum, assumenda impedit quaerat, voluptates dolorem! Magni libero autem porro commodi fugit?`
 let i = 0
-
+const speed = 100;
 function type(){
   if(i < text.length ){
-    p.append(text.charAt(i))
+     p.innerHTML += text.charAt(i)
     i++
-    setTimeout(type, 15)
+    setTimeout(type, speed)
   }
 }
 document.addEventListener('DOMContentLoaded', type)
