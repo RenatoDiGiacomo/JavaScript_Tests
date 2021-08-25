7af58728c73b9f2f5904e173243b7d08f03c4a07
RenatoDiGiacomo <renato_di_giacomo@hotmail.com>
2021-08-24 21:49:23 -0300

    New Processes Start

--- script.js
+++ script.js
@@ -1,38 +1,10 @@
-function initList() {
-    const menueach = function() {
-        const menuItems = [
-            "Counter",
-            "Hex",
-            "NavBar",
-            "Progress-Bar",
-            "Scrolling Bar Top",
-            "Typing Writer",
-        ];
-        let print = "";
-
-        menuItems.forEach(item => {
-            const text = item.replace(/\s/g, '-').toLowerCase()
-            print += `<li><a href="${text}/index.html">${item}</a></li>`;
-        })
-        return print;
-    };
-
-    const menu = document.getElementById("menu");
-    menu.innerHTML = menueach()
-
-}
-initList();
-
-const startmenu = document.querySelector('.startmenu');
-
-function menuselect() {
-    console.log('funciona')
-    const menu = document.getElementById('menu')
-    if (menu.classList.contains('active')) {
-        menu.classList.remove('active')
-    } else {
-        menu.classList.add('active')
-    }
-}
-
-startmenu.addEventListener('click', menuselect)
+const header = document.querySelector("#header ul");
+fetch("./folder.json")
+  .then((response) => response.json())
+  .then((data) =>
+    data.forEach((element) => {
+      var list = element.folder;
+      var url = element.url + "/index.html"
+      header.innerHTML += "<li><a href='"+url+"'>"+list+"</a></li>";
+    })
+  );
